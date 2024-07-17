9f1ac65f9f36a44c74fb9afe2788cb8d57ee47d7
Kaic Pierre <kaic.pierre@telefonicabigdata.onmicrosoft.com>
2024-07-16 23:51:56 -0300

    fix heroku configs

--- src/server.ts
+++ src/server.ts
@@ -30,10 +30,10 @@ const fastify = Fastify({
 
   const port: number = +!process.env.PORT
 
-  fastify.listen({ port: port || 3000 }, function (err, address) {
+  fastify.listen({ port: port || 3000, host: '0.0.0.0'}, function (err, address) {
     if (err) {
       fastify.log.error(err)
       process.exit(1)
-    }
+    },
     // Server is now listening on ${address}
   })
